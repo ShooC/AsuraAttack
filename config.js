7cd6d27d54eb08a12c9d895b609845b89daf663f
ShooC <118227993+ShooC@users.noreply.github.com>
2022-12-08 20:31:31 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'Asura'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6283177947582','6285731251228']
+global.premium = ['6283177947582']
+global.packname = 'Asura'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
